db05ddab659fff9f4903e6eec52df9de4423dbe2
stringm3x <stringwebmx@gmail.com>
2026-01-08 01:26:05 -0600

    chore: upgrade Next.js and patch vulnerabilities

--- app/Services/page.jsx
+++ app/Services/page.jsx
@@ -41,8 +41,8 @@ const pageServices = () => {
   }, []);
 
   return (
-    <section className="relative overflow-hidden flex flex-col py-10">
-      <div className="relative flex flex-col justify-center h-72 lg:h-80 xl:h-96">
+    <section className="relative overflow-hidden flex flex-col py-10 mx-auto 2xl:max-w-[1280px]">
+      <div className="relative flex flex-col justify-center py-20 ">
         <h1 className="relative text-6xl md:text-9xl font-ubuntu font-bold pl-5 md:pl-20 justify-self-center">
           SERVICIOS
         </h1>
@@ -50,34 +50,7 @@ const pageServices = () => {
 
       <div className="relative px-5 md:px-20 flex flex-col gap-10">
         {servicios.map(({ id, service, img, title2 }) => (
-          <div
-            className="card bg-white md:h-80 lg:w-3/4 rounded-2xl leading-[30px] lg:leading-[60px] flex flex-col md:flex-row py-10 pr-3"
-            key={id}
-          >
-            <div className="md:w-2/3 h-full">
-              <h1 className="text-bg font-ubuntu font-extrabold tracking-tight text-left text-4xl md:text-6xl lg:text-7xl leading-[30px] lg:leading-[60px] px-4 md:px-10">
-                {service}
-              </h1>
-              <h1 className="text-green font-ubuntu font-extrabold tracking-tight text-left text-4xl md:text-6xl lg:text-7xl leading-[30px] lg:leading-[60px] px-4 md:px-10">
-                {title2}
-              </h1>
-              <Image
-                src={img}
-                alt={service}
-                width={500}
-                height={500}
-                className="object-cover"
-              />
-            </div>
-
-            <div className="md:w-1/3 md:h-full self-end md:content-end">
-              <Link href={`/Services/${id}`} className="">
-                <Button className="xl:w-56 p-7 md:p-10 text-xl md:text-2xl rounded-full text-green bg-bg hover:text-bg hover:bg-green">
-                  Ver Servicio
-                </Button>
-              </Link>
-            </div>
-          </div>
+          <div key={id}></div>
         ))}
       </div>
     </section>
