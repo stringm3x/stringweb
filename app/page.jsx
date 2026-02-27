d5223f61a9f040e06e71ae911715f86a0c589cde
stringm3x <stringwebmx@gmail.com>
2026-02-27 03:41:45 -0600

    Version 2.0.2

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 // ✅ Usa rutas absolutas con @
-import Hero from "@/app/home/Hero";
-import Content from "@/app/home/Content";
-import Proyects from "@/app/home/Proyects";
-import Services from "@/app/home/Services";
-import Us from "@/app/home/Us";
-import Steps from "@/app/home/Steps";
+import Hero from "@/app/components/forms/home/Hero";
+import Content from "@/app/components/forms/home/Content";
+import Proyects from "@/app/components/forms/home/Proyects";
+import Services from "@/app/components/forms/home/Services";
+import Us from "@/app/components/forms/home/Us";
+import Steps from "@/app/components/forms/home/Steps";
 
 const Home = () => {
   return (
